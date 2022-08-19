8f5f84413473827f0746a832819cc0aa455a6125
yuzujoe <yuzun225@gmail.com>
2022-08-19 18:08:09 +0900

    newrelic apm を追加

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/yuzujoe/newrelic-zap-logs-in-context
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/newrelic/go-agent/v3 v3.18.1 // indirect
+	github.com/newrelic/go-agent/v3/integrations/nrgorilla v1.1.1 // indirect
+	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
+	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220817144833-d7fd3f11b9b1 // indirect
+	google.golang.org/grpc v1.48.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/newrelic/go-agent/v3/integrations/nrgorilla"
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 func main() {
 	r := mux.NewRouter()
+
+	app, err := makeNewRelicApplication()
+	if err != nil {
+		log.Fatalf("error init newrelic apm agent reason: %v", err)
+		os.Exit(1)
+	}
+
+	r.Use(nrgorilla.Middleware(app))
 
 	r.HandleFunc("/example/{name}", ExampleHandler)
 
@@ -19,4 +32,21 @@ func ExampleHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	log.Printf("name is: %v", vars["name"])
+}
+
+func makeNewRelicApplication() (*newrelic.Application, error) {
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("newrelic-zap-logs-in-context"),
+		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
+		newrelic.ConfigAppLogEnabled(false),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = app.WaitForConnection(5 * time.Second); err != nil {
+		return nil, err
+	}
+
+	return app, nil
 }
